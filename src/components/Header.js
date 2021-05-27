f760cca78b1d05bcd6ffd47e087f47f221625e25
adam040992 <adam040992@gmail.com>
2021-05-27 20:12:52 +0200

    Add: Material Ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
         <Container>
-            Header
+            <Logo>
+                <img src="/images/logo.svg" alt="Tesla Logo" />
+            </Logo>
+
+            <Menu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+            </Menu>
+
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <MenuIcon />
+            </RightMenu>
         </Container>
     )
 }
@@ -12,4 +28,41 @@ function Header() {
 export default Header
 
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    min-height: 60px;
+    position: fixed;
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    right: 0;
+    left: 0;
+`
+
+
+const Logo = styled.a``;
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+`;
+
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+    }
+`;
